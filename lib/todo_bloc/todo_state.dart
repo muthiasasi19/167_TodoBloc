58dd7872e05e59484599dde2e7dd35ed3272339f
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-07 14:13:55 +0700

    Buat state loaded untuk menyimpan daftar todo dan tanggal

--- lib/todo_bloc/todo_state.dart
+++ lib/todo_bloc/todo_state.dart
@@ -5,3 +5,10 @@ sealed class TodoState {}
 final class TodoInitial extends TodoState {}
 
 final class TodoLoading extends TodoState {}
+
+final class TodoLoaded extends TodoState {
+  final List<Todo> todos;
+  DateTime? selectDate;
+
+  TodoLoaded({required this.todos, required this.selectDate});
+}
