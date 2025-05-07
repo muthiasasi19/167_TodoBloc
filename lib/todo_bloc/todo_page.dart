280f6af67205e1a46054357110d66e3d141785c2
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-07 14:55:45 +0700

    Tambahkan form untuk input todo dengan validasi

--- lib/todo_bloc/todo_page.dart
+++ lib/todo_bloc/todo_page.dart
@@ -1,15 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tugas_bloc/todo_bloc/todo_bloc.dart';
-import 'package:flutter/material.dart';
-//import 'package:tugas_bloc/_bloc.dart';
 
 class TodoPage extends StatefulWidget {
   const TodoPage({super.key});
 
   @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  final _key = GlobalKey<FormState>();
+  final _controller = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    final _key = GlobalKey<FormState>();
-    final _controller = TextEditingController();
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -19,40 +24,60 @@ class TodoPage extends StatefulWidget {
               Text("Todo List"),
               Row(
                 children: [
-                  Text("Selected Date"),
+                  Text("Selected Date: "),
                   BlocBuilder<TodoBloc, TodoState>(
                     builder: (context, state) {
-                      if (state is TodoLoaded) {
-                        if (state.selectDate != null) {
-                          return Text(
-                            '${state.selectDate!.day}/${state.selectDate!.month}/${state.selectDate!.year}',
-                          );
-                        }
+                      if (state is TodoLoaded && state.selectDate != null) {
+                        return Text(
+                          '${state.selectDate!.day}/${state.selectDate!.month}/${state.selectDate!.year}',
+                        );
                       }
                       return Text("No date selected");
                     },
                   ),
-                ],
-              ),
-              SizedBox(width: 16.0),
-              Expanded(
-                child:ElevatedButton(
-                  onPressed: () {
-                    showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                       firstDate: DateTime(2000), 
-                       lastDate: DateTime(2000),
-                       ).then(selectedDate) {
+                  SizedBox(width: 16.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2100),
+                      ).then((selectedDate) {
                         if (selectedDate != null) {
                           context.read<TodoBloc>().add(
                             TodoSelectDate(date: selectedDate),
                           );
                         }
-                       }
-                  },
-                  child: Text('Select Date')) 
-                  ), 
+                      });
+                    },
+                    child: Text('Select Date'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),
+              Form(
+                key: _key,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          labelText: 'Todo',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please enter a todo";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
