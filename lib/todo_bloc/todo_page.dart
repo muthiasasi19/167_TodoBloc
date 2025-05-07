8666bbab9bc52e6c74c029f1e6ef5309af2c8931
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-07 20:25:58 +0700

    Tambahkan status todo completed atau belum completed.

--- lib/todo_bloc/todo_page.dart
+++ lib/todo_bloc/todo_page.dart
@@ -134,6 +134,18 @@ class _TodoPageState extends State<TodoPage> {
                                       '${todo.date.day}/${todo.date.month}/${todo.date.year}',
                                       style: TextStyle(color: Colors.grey),
                                     ),
+                                    SizedBox(height: 4.0),
+                                    Text(
+                                      todo.isCompleted
+                                          ? 'Completed'
+                                          : 'Not Completed',
+                                      style: TextStyle(
+                                        color:
+                                            todo.isCompleted
+                                                ? Colors.green
+                                                : Colors.red,
+                                      ),
+                                    ),
                                   ],
                                 ),
                               ],
