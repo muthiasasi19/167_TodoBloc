90e8b048953271627157394e762e6661d01a2516
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-07 14:09:14 +0700

    Tambahkan event untuk memilih tanggal pada todo.

--- lib/todo_bloc/todo_event.dart
+++ lib/todo_bloc/todo_event.dart
@@ -13,3 +13,9 @@ final class TodoEventComplete extends TodoEvent {
 
   TodoEventComplete({required this.index});
 }
+
+final class TodoSelectDate extends TodoEvent {
+  final DateTime date;
+
+  TodoSelectDate({required this.date});
+}
