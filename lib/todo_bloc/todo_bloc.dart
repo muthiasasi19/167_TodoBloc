4c319bfba051ec3f1f068e2a777e6f2ccec9683e
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-07 14:19:00 +0700

    Buat BLoC untuk menangani penambahan todo

--- lib/todo_bloc/todo_bloc.dart
+++ lib/todo_bloc/todo_bloc.dart
@@ -4,3 +4,19 @@ import 'package:tugas_bloc/model/todo.dart';
 
 part 'todo_event.dart';
 part 'todo_state.dart';
+
+class TodoBloc extends Bloc<TodoEvent, TodoState> {
+  TodoBloc() : super(TodoLoaded(todos: [], selectDate: null)) {
+    on<TodoEventAdd>((event, emit) {
+      final currentState = state;
+      if (currentState is TodoLoaded) {
+        final List<Todo> updateTodos = List.from(currentState.todos);
+        updateTodos.add(
+          Todo(title: event.title, isCompleted: false, date: event.date),
+        );
+
+        emit(
+          TodoLoaded(todos: updateTodos, selectDate: currentState.selectDate),
+        );
+      }
+    });
